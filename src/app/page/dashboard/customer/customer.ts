a9f6323e642cfc6cc1e528131074a29f33912b04
Asinsala2001 <112684739+Asinsala2001@users.noreply.github.com>
2026-02-18 14:36:31 +0530

    feat: add initial structure for GrocerGo application with Home, Login, Item, and Order components
    - Created Home component with corresponding HTML, CSS, and spec files.
    - Developed Login component with its HTML, CSS, and spec files.
    - Implemented Item component with its HTML, CSS, and spec files.
    - Added Order component with its HTML, CSS, and spec files.
    - Established main application entry point and index HTML.
    - Configured TypeScript settings for the application.
    - Included global styles and imported necessary libraries.

---
+++ src/app/page/dashboard/customer/customer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-customer',
+  imports: [RouterOutlet],
+  templateUrl: './customer.html',
+  styleUrl: './customer.css',
+})
+export class Customer {
+
+}
---
+++ src/app/page/dashboard/item/item.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-item',
+  imports: [RouterOutlet],
+  templateUrl: './item.html',
+  styleUrl: './item.css',
+})
+export class Item {
+
+}
